dc96fc94be06ba833f96c00aeedb21776ccfdaf7
Connor Hood <chood531@gmail.com>
2016-04-15 21:45:35 -0500

    ...

--- public/wp-content/themes/base-theme/functions.php
+++ public/wp-content/themes/base-theme/functions.php
@@ -21,5 +21,6 @@ add_filter('upload_mimes', 'custom_upload_mimes');
 function custom_upload_mimes ( $existing_mimes=array() ) {
     // add your extension to the array
     $existing_mimes['vcf'] = 'text/x-vcard';
+    $existing_mimes['svg'] = 'image/svg+xml';
     return $existing_mimes;
 }
