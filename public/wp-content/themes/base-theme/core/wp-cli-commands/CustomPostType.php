30ff7187e3d0822c1a0a8677db5769da3c81fac7
Buzdyk Gregory <buzdyk.gregory@gmail.com>
2017-06-19 15:42:23 +0300

    add flush rewrites command

--- public/wp-content/themes/base-theme/core/wp-cli-commands/CustomPostType.php
+++ public/wp-content/themes/base-theme/core/wp-cli-commands/CustomPostType.php
@@ -5,8 +5,8 @@ class CustomPostType_Command extends WP_CLI_Command
     /**
      * Sync ACF groups which were changed extenally (e.g. by another developer)
      */
-    public function updateRewrites()
+    public function flushRewrites()
     {
-
+        flush_rewrite_rules();
     }
 }
